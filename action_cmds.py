cafa9d42fa810509bea27cb0156d96203db2b8bd
guillermooo <guillermo.lopez@outlook.com>
2014-03-29 09:48:21 +0100

    enable `cs` and `ds`

--- action_cmds.py
+++ action_cmds.py
@@ -50,68 +50,71 @@ class ViSurround(ViOperatorDef):
         return cmd
 
 
-# def validate_vi_plug_y_s(in_):
-#     single = len(in_) == 1 and in_ != '<'
-#     tag = re.match('<.*?>', in_)
-#     return single or tag
+@plugins.register(keys=[('ds', (modes.NORMAL, modes.OPERATOR_PENDING))])
+class ViDeleteSurround(ViOperatorDef):
+    def __init__(self, *args, **kwargs):
+        ViOperatorDef.__init__(self, *args, **kwargs)
+
+        self.scroll_into_view = True
+        self.updates_xpos = True
+        self.repeatable = True
+
+        self.input_parser = parser_def(
+                            command=inputs.one_char,
+                            interactive_command=None,
+                            input_param=None,
+                            on_done=None,
+                            type=input_types.INMEDIATE)
+
+    @property
+    def accept_input(self):
+        single = len(self.inp) == 1 and self.inp != '<'
+        tag = re.match('<.*?>', self.inp)
+        return not(single or tag)
+
+    def accept(self, key):
+        self._inp += utils.translate_char(key)
+        return True
+
+    def translate(self, state):
+        cmd = {}
+        cmd['action'] = '_vi_plug_d_s'
+        cmd['action_args'] = {'mode': state.mode,
+                              'replace_what': self.inp}
+        return cmd
 
 
-# def validate_vi_plug_c_s(in_):
-#     return len(in_) == 2
+@plugins.register(keys=[('cs', (modes.NORMAL, modes.OPERATOR_PENDING))])
+class ViChangeSurround(ViOperatorDef):
+    def __init__(self, *args, **kwargs):
+        ViOperatorDef.__init__(self, *args, **kwargs)
 
+        self.scroll_into_view = True
+        self.updates_xpos = True
+        self.repeatable = True
 
-# def validate_vi_plug_d_s(in_):
-#     return len(in_) == 1
+        self.input_parser = parser_def(
+                            command=inputs.one_char,
+                            interactive_command=None,
+                            input_param=None,
+                            on_done=None,
+                            type=input_types.INMEDIATE)
 
+    @property
+    def accept_input(self):
+        return len(self.inp) != 2
 
-# def do_post_load():
-#     from Vintageous.state import plugin_manager
+    def accept(self, key):
+        self._inp += utils.translate_char(key)
+        return True
 
-#     # register action for action parsing
-#     @plugin_manager.register_action
-#     def vi_plug_y_s(vi_cmd_data):
-#         vi_cmd_data['action']['command'] = '_vi_plug_y_s'
-#         vi_cmd_data['action']['args'] = {'mode': vi_cmd_data['mode'], 'surround_with': vi_cmd_data['user_action_input']}
-#         vi_cmd_data['follow_up_mode'] = 'vi_enter_normal_mode'
+    def translate(self, state):
+        cmd = {}
+        cmd['action'] = '_vi_plug_c_s'
+        cmd['action_args'] = {'mode': state.mode,
+                              'replace_what': self.inp}
+        return cmd
 
-#         return vi_cmd_data
-
-
-#     @plugin_manager.register_action
-#     def vi_plug_c_s(vi_cmd_data):
-#         vi_cmd_data['motion_required'] = False
-#         vi_cmd_data['action']['command'] = '_vi_plug_c_s'
-#         vi_cmd_data['action']['args'] = {'mode': vi_cmd_data['mode'], 'replace_what': vi_cmd_data['user_action_input']}
-#         vi_cmd_data['motion']['command'] = '_vi_no_op'
-#         vi_cmd_data['motion']['args'] = {}
-#         vi_cmd_data['follow_up_mode'] = 'vi_enter_normal_mode'
-
-#         return vi_cmd_data
-
-
-#     @plugin_manager.register_action
-#     def vi_plug_d_s(vi_cmd_data):
-#         vi_cmd_data['motion_required'] = False
-#         vi_cmd_data['action']['command'] = '_vi_plug_d_s'
-#         vi_cmd_data['action']['args'] = {'mode': vi_cmd_data['mode'], 'replace_what': vi_cmd_data['user_action_input']}
-#         vi_cmd_data['motion']['command'] = '_vi_no_op'
-#         vi_cmd_data['motion']['args'] = {}
-#         vi_cmd_data['follow_up_mode'] = 'vi_enter_normal_mode'
-
-#         return vi_cmd_data
-
-
-#     plugin_manager.register_composite_command({('vi_y', 'vi_s'): ('vi_plug_y_s', DIGRAPH_ACTION)})
-#     plugin_manager.register_action_input_parser({'vi_plug_y_s': (INPUT_AFTER_MOTION, validate_vi_plug_y_s)})
-
-#     plugin_manager.register_composite_command({('vi_c', 'vi_s'): ('vi_plug_c_s', DIGRAPH_ACTION)})
-#     plugin_manager.register_action_input_parser({'vi_plug_c_s': (INPUT_IMMEDIATE, validate_vi_plug_c_s)})
-
-#     plugin_manager.register_composite_command({('vi_d', 'vi_s'): ('vi_plug_d_s', DIGRAPH_ACTION)})
-#     plugin_manager.register_action_input_parser({'vi_plug_d_s': (INPUT_IMMEDIATE, validate_vi_plug_d_s)})
-
-# add key bindings in .sublime-keymap file
-# (none in this case: ys<arg>; y and s are standard Vim commands defined in Vintageous)
 
 # actual command implementation
 class _vi_plug_y_s(ViTextCommandBase):
@@ -167,7 +170,7 @@ class _vi_plug_c_s(sublime_plugin.TextCommand):
     }
     def run(self, edit, mode=None, replace_what=''):
         def f(view, s):
-            if mode == _MODE_INTERNAL_NORMAL:
+            if mode == modes.INTERNAL_NORMAL:
                 self.replace(edit, s, replace_what)
                 return s
             return s
@@ -207,7 +210,7 @@ class _vi_plug_d_s(sublime_plugin.TextCommand):
     }
     def run(self, edit, mode=None, replace_what=''):
         def f(view, s):
-            if mode == _MODE_INTERNAL_NORMAL:
+            if mode == modes.INTERNAL_NORMAL:
                 self.replace(edit, s, replace_what)
                 return s
             return s
